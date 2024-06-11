44e3b4f9ba72b5876e36cb6281aeae598498ced8
Tomas Turlik <tomas.turlik@gmail.com>
2024-06-11 16:50:43 +0200

    Add parent questions to subquestion generation

--- amplify/amplify.py
+++ amplify/amplify.py
@@ -8,26 +8,44 @@ Question = str
 Answer = str
 Subs = list[tuple[Question, Answer]]
 
-def make_subquestion_prompt(question: str) -> str:
-    return F(
+
+def make_subquestion_prompt(
+    question: str, parent_questions: list = [], last_k_questions: int = 10
+) -> str:
+
+    if last_k_questions > 0:
+        parent_questions = parent_questions[-last_k_questions:]
+    prompt = F(
         f"""Decompose the following question into 2-5 subquestions that would help you answer the question. Make the questions stand alone, so that they can be answered without the context of the original question.
 
 Question: "{question}"
 Subquestions:
 -"""
     ).strip()
+    if len(parent_questions) > 1:
+        prefix = (
+            F(
+                "The following Question was generated based on following parent questions, in decomposing and answering take consideration of them. \n "
+            )
+            + F("\n").join(F(f"- {q}") for q in parent_questions)
+            + F("\n\n")
+        )
+        prompt = prefix + prompt
+    return prompt
 
 
 async def ask_subquestions(
     question: str = "What is the effect of creatine on cognition?",
+    parent_questions: list[str] = [],
 ):
-    prompt = make_subquestion_prompt(question)
-    subquestions_text = await recipe.agent().complete(prompt=prompt, max_tokens=512)
+    prompt = make_subquestion_prompt(question, parent_questions=parent_questions)
+    subquestions_text = await recipe.agent().complete(prompt=prompt)
     subquestions = [line.strip("- ") for line in subquestions_text.split("\n")]
     return subquestions
 
 
 recipe.main(ask_subquestions)
+
 
 def render_background(subs: Subs) -> str:
     if not subs:
@@ -47,8 +65,10 @@ Answer: "
     ).strip()
 
 
-async def get_subs(question: str, depth: int) -> Subs:
-    subquestions = await ask_subquestions(question=question)
+async def get_subs(question: str, depth: int, parent_questions: list[str] = []) -> Subs:
+    subquestions = await ask_subquestions(
+        question=question, parent_questions=parent_questions
+    )
     subanswers = await map_async(
         subquestions, lambda q: answer_by_amplification(question=q, depth=depth)
     )
@@ -56,11 +76,21 @@ async def get_subs(question: str, depth: int) -> Subs:
 
 
 async def answer_by_amplification(
-    question: str = "What is the effect of creatine on cognition?", depth: int = 1
+    question: str = "What is the effect of creatine on cognition?",
+    depth: int = 2,
+    parent_questions: list[str] = [],
 ):
-    subs = await get_subs(question, depth - 1) if depth > 0 else []
+    if not parent_questions:
+        parent_questions = [question]
+    else:
+        parent_questions.append(question)
+    subs = (
+        await get_subs(question, depth - 1, parent_questions=parent_questions)
+        if depth > 0
+        else []
+    )
     prompt = make_qa_prompt(question, subs=subs)
-    answer = await recipe.agent().complete(prompt=prompt, stop='"', max_tokens=512)
+    answer = await recipe.agent().complete(prompt=prompt, stop='"')
     return answer
 
 
